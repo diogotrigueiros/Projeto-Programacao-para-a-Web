0d1126a10992ad1ca90cd601d0f138ca96785d88
Diogo Trigueiros <121031730+diogotrigueiros@users.noreply.github.com>
2023-05-29 15:41:14 +0100

    Delete Apex - Diogo Trigueiros directory

--- Apex - Diogo Trigueiros/script.js
+++
@@ -1,6 +0,0 @@
-const toggleMenu = document.querySelector('.toggle-menu');
-const menu = document.querySelector('.menu');
-
-toggleMenu.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
